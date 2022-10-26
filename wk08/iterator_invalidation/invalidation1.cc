bebf088d177c331a22b8d642a3aeabb7d4dfbaf4
Onni Vitikainen <onni.vitikainen@tuni.fi>
2022-10-26 19:18:14 +0300

    invalidation1

--- wk08/iterator_invalidation/invalidation1.cc
+++ wk08/iterator_invalidation/invalidation1.cc
@@ -12,6 +12,7 @@ using namespace std;
  */
 std::vector<int> ascendingVector(int n) {
     std::vector<int> v;
+    v.reserve(n);
     auto beg1 = v.begin();
     for (int i = 0; i < n; ++i) { v.insert(beg1+i, i); }
 
