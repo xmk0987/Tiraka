522e10c91ce8a59ae1a322f8d6ab92f0ec2cf438
Onni Vitikainen <onni.vitikainen@tuni.fi>
2022-11-02 23:52:05 +0200

    improve2

--- wk09/improving_functions/improve2.cc
+++ wk09/improving_functions/improve2.cc
@@ -11,10 +11,8 @@
  */
 int minValue(std::vector<int> vec){
     if ( vec.empty() ) { return 0; }
-    if ( std::is_sorted(vec.begin(), vec.end()) ) {
-        return vec[0];
-    } else {
-        std::sort(vec.begin(), vec.end());
-        return vec[0];
+    else{
+        return *std::min_element(vec.begin(),vec.end());
     }
+
 }
